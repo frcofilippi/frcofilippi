8d5cb7d64d1aed5930ec7bceeef6f4286a716282
Franco Filippi <frcofilippi@gmail.com>
2022-05-11 19:26:48 -0300

    changed action scope

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ const today = new Date();
 const newTemplate = readmeTeplate.replace('{{Date}}',`${today.getFullYear()}/${today.getMonth()}/${today.getDate()}`);
 await fs.writeFile('./README.md', newTemplate);
 })();
-
-console.log('Profile updated!');
