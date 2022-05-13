e4728319efb2f8af3824325bf69d735f21aedc35
Franco Filippi <frcofilippi@gmail.com>
2022-05-12 21:14:00 -0300

    test

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const fs = require("fs").promises;
 const axios = require("./services");
 
+const API_KEY = process.env.API_KEY;
+
 (async () => {
-  const response = await axios.get('v1/current.json',{
-    params: {
-      key: process.env.API_KEY,
-    // key: '2b794e84f2a84c6b9af194929221105',
-      q: "Cordoba",
-      aqi: "yes",
-    },
-  });
+  try {
+    const response = await axios.get("v1/current.json", {
+      params: {
+        key: API_KEY,
+        // key: '2b794e84f2a84c6b9af194929221105',
+        q: "Cordoba",
+        aqi: "yes",
+      },
+    });
+    const { location, current } = response.data;
 
-  const {location, current} = response.data
-
-  const readmeTeplate = await fs.readFile("./README.md.tpl", "utf-8");
-  const today = new Date();
-  const newTemplate = readmeTeplate.replace(
-    "{{Date}}",
-    `${today.getFullYear()}/${today.getMonth()}/${today.getDate()}`
-  ).replace('{{Temperature}}', `Location: ${location.name} - Temperature: ${current.temp_c}ºC`);
-  await fs.writeFile("./README.md", newTemplate);
+    const readmeTeplate = await fs.readFile("./README.md.tpl", "utf-8");
+    const today = new Date();
+    const newTemplate = readmeTeplate
+      .replace(
+        "{{Date}}",
+        `${today.getFullYear()}/${today.getMonth()}/${today.getDate()}`
+      )
+      .replace(
+        "{{Temperature}}",
+        `Location: ${location.name} - Temperature: ${current.temp_c}ºC`
+      );
+    await fs.writeFile("./README.md", newTemplate);
+  } catch (error) {
+    console.error(`${API_KEY}`);
+  }
 })();
