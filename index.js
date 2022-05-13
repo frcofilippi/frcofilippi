62dd30716969af60fe52d379468d0cd60e1c71d9
Franco Filippi <frcofilippi@gmail.com>
2022-05-13 17:10:50 -0300

    refactor index

--- index.js
+++ index.js
@@ -5,29 +5,34 @@ const API_KEY = process.env.API_KEY;
 
 (async () => {
   try {
-    const response = await axios.get("v1/current.json", {
-      params: {
-        key: API_KEY,
-        // key: '2b794e84f2a84c6b9af194929221105',
-        q: "Cordoba",
-        aqi: "yes",
-      },
-    });
-    const { location, current } = response.data;
-
     const readmeTeplate = await fs.readFile("./README.md.tpl", "utf-8");
     const today = new Date();
+    const weather = await fetchWeatherInfo(API_KEY, "cordoba");
     const newTemplate = readmeTeplate
       .replace(
         "{{Date}}",
         `${today.getFullYear()}/${today.getMonth()}/${today.getDate()}`
       )
-      .replace(
-        "{{Temperature}}",
-        `Location: ${location.name} - Temperature: ${current.temp_c}ºC`
-      );
+      .replace("{{Temperature}}", weather);
+
     await fs.writeFile("./README.md", newTemplate);
   } catch (error) {
     console.error(`API_KEY could not be read - value: ${API_KEY}`);
   }
 })();
+
+const fetchWeatherInfo = async (key, city) => {
+  try {
+    const response = await axios.get("v1/current.json", {
+      params: {
+        key: key,
+        q: city,
+        aqi: "yes",
+      },
+    });
+    const { location, current } = rsponse.data;
+    return `Location: ${location.name} - Temperature: ${current.temp_c}ºC`;
+  } catch (error) {
+    return "";
+  }
+};
